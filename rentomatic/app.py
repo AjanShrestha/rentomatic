9278cf195fd3e3a3a0282b2e489577423a7e2b36
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-27 16:29:01 +1000

    feat: Add app file
    - initialises the Flask app
    - configures it
    - registers the blueprints

---
+++ rentomatic/app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+from rentomatic.rest import room
+from rentomatic.flask_settings import DevConfig
+
+
+def create_app(config_object=DevConfig):
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+    app.register_blueprint(room.blueprint)
+    return app
