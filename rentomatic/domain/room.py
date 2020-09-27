ffb93fa8ee1f3c5651b10d25f1cd3e647b0e46d4
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-27 13:34:00 +1000

    feat: Domain models - Compare rooms via dict structure

--- rentomatic/domain/room.py
+++ rentomatic/domain/room.py
@@ -27,3 +27,6 @@ class Room:
             "longitude": self.longitude,
             "latitude": self.latitude,
         }
+
+    def __eq__(self, other):
+        return self.to_dict() == other.to_dict()
