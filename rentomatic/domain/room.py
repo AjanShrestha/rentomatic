c46fd6bc384264c5fb85e2acbd4906fa54d5744c
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-27 13:21:03 +1000

    feat: Domain models - Room

---
+++ rentomatic/domain/room.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+class Room:
+    def __init__(self, code, size, price, longitude, latitude):
+        self.code = code
+        self.size = size
+        self.price = price
+        self.longitude = longitude
+        self.latitude = latitude
