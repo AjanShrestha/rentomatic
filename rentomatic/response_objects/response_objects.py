bfc86f98f5aa135c0a16d41a13bbc6f362478e5f
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-09-28 20:01:58 +1000

    feat: Response success and type

--- rentomatic/response_objects/response_objects.py
+++ rentomatic/response_objects/response_objects.py
@@ -2,7 +2,10 @@
 
 
 class ResponseSuccess:
+    SUCCESS = "Success"
+
     def __init__(self, value=None):
+        self.type = self.SUCCESS
         self.value = value
 
     def __bool__(self):
--- tests/request_objects/test_response_objects.py
+++ tests/request_objects/test_response_objects.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
+import pytest
+
 from rentomatic.response_objects import response_objects as res
+
+
+@pytest.fixture
+def response_value():
+    return {"key": ["value1", "value2"]}
 
 
 def test_response_success_is_true():
     assert bool(res.ResponseSuccess()) is True
+
+
+def test_response_success_has_type_and_value(response_value):
+    response = res.ResponseSuccess(response_value)
+
+    assert response.type == res.ResponseSuccess.SUCCESS
+    assert response.value == response_value
